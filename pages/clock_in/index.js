daa512d42ebc9b163f23f5d7846b62f2541e173c
momo <xirizhifeng@163.com>
2021-03-16 12:32:11 +0800

    calendar

--- pages/clock_in/index.js
+++ pages/clock_in/index.js
@@ -5,7 +5,19 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    calendarConfig: {
+      weekMode: true, // 周视图模式
+      theme: 'default', // 日历主题，目前共两款可选择，默认 default 及 elegant，自定义主题色在参考 /theme 文件夹
+      inverse: true, // 单选模式下是否支持取消选中,
+      markToday: '今', // 当天日期展示不使用默认数字，用特殊文字标记
+      takeoverTap: true, // 是否完全接管日期点击事件（日期不会选中)
+      emphasisWeek: true, // 是否高亮显示周末日期
+      highlightToday: true, // 是否高亮显示当天，区别于选中样式（初始化时当天高亮并不代表已选中当天）
+      preventSwipe: true, // 是否禁用日历滑动切换月份
+      firstDayOfWeek: 'Mon', // 每周第一天为周一还是周日，默认按周日开始
+      onlyShowCurrentMonth: true, // 日历面板是否只显示本月日期
+      autoChoosedWhenJump: true, // 设置默认日期及跳转到指定日期后是否需要自动选中
+    }
   },
 
   /**
@@ -14,7 +26,21 @@ Page({
   onLoad: function (options) {
 
   },
-
+  /**
+    * 日期点击事件（此事件会完全接管点击事件），需自定义配置 takeoverTap 值为真才能生效
+    * currentSelect 当前点击的日期
+    */
+  takeoverTap(e) {
+    console.log('takeoverTap', e.detail) // => { year: 2019, month: 12, date: 3, ...}
+  },
+  /**
+   * 当改变月份时触发
+   * => current 当前年月 / next 切换后的年月
+   */
+  whenChangeMonth(e) {
+    console.log('whenChangeMonth', e.detail)
+    // => { current: { month: 3, ... }, next: { month: 4, ... }}
+  },
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
