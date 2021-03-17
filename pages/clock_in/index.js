f9f669657e4e1d6e443175b70428daab393ec8c2
momo <xirizhifeng@163.com>
2021-03-17 09:38:25 +0800

    bug fixed

--- pages/clock_in/index.js
+++ pages/clock_in/index.js
@@ -1,4 +1,9 @@
 // pages/clock_in/index.js
+import todo from "../../components/calendar/plugins/todo";
+import plugin from '../../components/calendar/plugins/index'
+
+plugin
+  .use(todo)
 Page({
 
   /**
@@ -24,7 +29,6 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
   },
   /**
     * 日期点击事件（此事件会完全接管点击事件），需自定义配置 takeoverTap 值为真才能生效
@@ -46,6 +50,28 @@ Page({
    */
   onReady: function () {
 
+    const calendar = this.selectComponent('#calendar').calendar;
+    calendar.setTodos({
+      // 待办点标记设置
+      pos: 'bottom', // 待办点标记位置 ['top', 'bottom']
+      dotColor: 'purple', // 待办点标记颜色
+      circle: true, // 待办圆圈标记设置（如圆圈标记已签到日期），该设置与点标记设置互斥
+      showLabelAlways: true, // 点击时是否显示待办事项（圆点/文字），在 circle 为 true 及当日历配置 showLunar 为 true 时，此配置失效
+      dates: [
+        {
+          year: 2021,
+          month: 3,
+          date: 1,
+          todoText: '待办',
+          color: '#f40' // 单独定义代办颜色 (标记点、文字)
+        },
+        {
+          year: 2021,
+          month: 3,
+          date: 15
+        }
+      ]
+    })
   },
 
   /**
